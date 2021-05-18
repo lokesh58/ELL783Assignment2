a7254559436e92effa0cfe247129f8392bd67fbe
Lokesh Patel <patellokesh05899@gmail.com>
2021-05-18 11:15:08 +0530

    inituvm doesn't write metadata

--- xv6-public-fresh/vm.c
+++ xv6-public-fresh/vm.c
@@ -328,7 +328,7 @@ inituvm(pde_t *pgdir, char *init, uint sz)
     panic("inituvm: more than a page");
   mem = kalloc();
   memset(mem, 0, PGSIZE);
-  mappages(pgdir, 0, PGSIZE, V2P(mem), PTE_W|PTE_U, 1);
+  mappages(pgdir, 0, PGSIZE, V2P(mem), PTE_W|PTE_U, 0);
   memmove(mem, init, sz);
 }
 
